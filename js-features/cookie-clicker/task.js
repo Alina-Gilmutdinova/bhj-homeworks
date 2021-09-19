d6917fc84e647bf64b9c967861a8e6f92ae4778f
Alina Gilmutdinova <a_gilmutdinova21@bk.ru>
2021-09-19 18:34:08 +0300

    вопросы по игре печенька

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,8 +1,20 @@
-let cookie = document.getElementById("cookie"); //подписалась на печеньку
-let clicker__cookie = 0; //начальное значение счетчика 0
 
-function clickMe() {
+const clicker__cookie = document.getElementById("clicker__counter"); // нашли счетчик
+clicker__cookie.textContent = "0";  //?? не понятно зачем нужен textContent и что записывать после =
+
+const cookie = document.getElementById("cookie"); //подписалась на печеньку
+cookie.onclick = function() {
    clicker__cookie += 1; //увеличиваем значение счетчика на 1 при клике
 };
 
-cookie.onclick = clickMe;
+
+
+cookieSize();
+function cookieSize() { //изменяем размер печеньки
+   const image = document.getElementById("cookie");
+   if (cookie.onclick % 2) { //если нажатие четное количество раз
+      image.width = 300; //увеличиваем картинку
+   } else {
+      image.width = 200; //уменьшаем
+   }
+}
