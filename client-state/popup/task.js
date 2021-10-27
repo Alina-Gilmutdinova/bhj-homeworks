2a3bce9f4730739e2b3d8e232cca1c72055ed499
Alina Gilmutdinova <a_gilmutdinova21@bk.ru>
2021-10-27 20:39:14 +0300

    хранение состояния на сервере

---
+++ client-state/popup/task.js
@@ -0,0 +1,24 @@
+//Необходимо реализовать всплывающее окно, которое появляется только один раз
+const modal = document.getElementById("subscribe-modal");
+const times = document.querySelector(".modal__close_times");
+const key = "modal_active";
+
+const getCookie = (name) => {
+    const value = document.cookie;
+    const str = value.split(";").find((item) => item.startsWith(name + "="));
+    if (str && str.includes("=")) {
+        return str.split("=").pop();
+    }
+    return "";
+}
+
+times.onclick = () => {
+    modal.classList.remove(key);
+    document.cookie = `${key}=active`;
+}
+
+if (getCookie(key) === "active") {
+    modal.classList.remove(key);
+} else {
+    modal.classList.add(key);
+}
