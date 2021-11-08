8e79587db26ba62fb252056eaf7231452299e00e
Alina Gilmutdinova <a_gilmutdinova21@bk.ru>
2021-11-08 22:19:52 +0300

    правка задачи с меню

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,24 +1,22 @@
 let classLinks = document.querySelectorAll(".menu__link");//обращаемся как в сss
 
 for (let menuLink of classLinks) {
-   menuLink.onclick = function () {
-      let menuLinkParent = menuLink.closest("li");
+    menuLink.onclick = function () {
+        let menuLinkParent = menuLink.closest("li");
+    let activeMenuItem = document.querySelector(".menu_active");
 
-		let menuSub = menuLinkParent.querySelector(".menu__item > .menu_sub");
+    let menuSub = menuLinkParent.querySelector(".menu__item > .menu_sub");
+        let childrenMenuSub = menuLinkParent.querySelector(".menu_active");
 
-		const activeMenuItem = document.querySelector(".menu_active");
+        if (activeMenuItem !== null) {
+            activeMenuItem.classList.remove("menu_active");
+        }
 
-		if (menuSub !== null && activeMenuItem !== null && menuSub === activeMenuItem) { //проверяем присутствие элемента
-			menuSub.classList.remove("menu_active");
-			return false; //чтобы обработчик не выполнялся
-		}
+        if (menuSub !== null && childrenMenuSub === null) {
+            menuSub.classList.toggle("menu_active");
+        }
 
-		if (activeMenuItem && activeMenuItem !== null) {
-			activeMenuItem.classList.remove("menu_active");
-		}
-		menuSub.classList.toggle("menu_active");
-
-		return false;
-
-	};
+        return false;
+    };
 };
+
